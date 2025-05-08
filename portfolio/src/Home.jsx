41888b40e6d0110fba6e91198ee0b0bd66884500
JD-NT <johndale.arevalo@gmail.com>
2025-05-08 20:02:24 +0800

    first commit

---
+++ portfolio/src/Home.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function Home() { return (
+    <>
+    <section id="home">
+    <div class="heronav">
+        <div class="herocontainer">
+            <h1>World Name</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, modi animi. Voluptates, necessitatibus culpa ut reiciendis earum eos veritatis, rem ex alias quaerat repudiandae dolorum enim vitae esse ipsum? Soluta.</p>
+            <a href="#" class="createbuttonhero">Create a character</a>
+        </div>
+    </div>
+    </section>
+    </>
+    )
+}
+
+export default Home
---
+++ portfolio/src/NavBar.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function NavBar() { return (
+    <>
+ <nav>
+        <label class="logo">World Name</label>
+        <ul>
+            <li ><a href="#home">Home</a></li>
+            <li ><a href="#intro">Introduction</a></li>
+            <li><a href="#projects">Project</a></li>
+            <li><a href="#about">About</a></li>
+            <li ><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+    </>
+    )}
+
+export default NavBar
