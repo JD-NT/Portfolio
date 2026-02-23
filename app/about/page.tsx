a2ae32b859c70a664d8d300141087b0f686f86a0
JD-NT <johndale.arevalo@gmail.com>
2026-02-23 19:17:03 +0800

    made the about page responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,18 +10,21 @@ function About() {
             <div className='about-layout'>
                 <div className='about-me1'>
                     <div>
-                    <Image className='bounding-box-L' src='./assets/BunnyIcon.svg' width={400} height={650} alt=''/> <small>You can call me: Dale, Bon</small>
+                        <div className='bounding-box-L'> <Image className='' src='/assets/MePixel.png' width={400} height={650} alt=''/> </div>
+                        <small>You can call me: Dale, Bon</small>
                     </div>
                 </div>
                 <div className='about-me2'>
                     <div className='about-description'>
-                        <h6>Background</h6>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, sunt. Eos libero magni fugit dolores nemo optio eius explicabo, velit quisquam expedita ipsa dignissimos, eaque delectus exercitationem iure accusantium nostrum!</p>
+                        <h6>Background:</h6>
+                        <small>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, sunt. Eos libero magni fugit dolores nemo optio eius explicabo, velit quisquam expedita ipsa dignissimos, eaque delectus exercitationem iure accusantium nostrum!</small>
                         <br />
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, sunt. Eos libero magni fugit dolores nemo optio eius explicabo, velit quisquam expedita ipsa dignissimos, eaque delectus exercitationem iure accusantium nostrum!</p>
+                        <br />
+                        <small>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, sunt. Eos libero magni fugit dolores nemo optio eius explicabo, velit quisquam expedita ipsa dignissimos, eaque delectus exercitationem iure accusantium nostrum!</small>
                     </div>
                     <div className='about-hobbies'>
-                        <h6>Hobbies</h6>
+                        <h6>Other things I can do:</h6>
+                        <small>Pixel Art, Graphic Design, Wireframing, Programming</small>
                     </div>
                 </div>
             </div>
